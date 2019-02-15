ad4d09c18e7aac90459a9e2ac28c7ea9184d4115
Anmol Khandekar <anmol.khandekar92@gmail.com>
2019-02-15 00:57:57 -0800

    changed the resume and editted aboutme and title page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -10,7 +10,7 @@ class AboutMe extends Component {
                         <h2>About Me</h2>
                         <p style={{ width: '75%' , margin: 'auto', paddingTop: '1em'}}>
                         If you want to know more about me please do contact me via refering to the contact page.<br/>
-                        And I have made my Protofolio using React and React-MDL, you can click on the github link to see the code.
+                        And I have made my Protfolio using React and React-MDL, you can click on the github link to see the code.
                         </p>
                         <a href="https://github.com/anmol091192/myProtfolio"  rel="noopener noreferrer" target="_blank">
                             <Button colored>GitHub</Button>
